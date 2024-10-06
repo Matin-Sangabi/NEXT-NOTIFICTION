a2ee0bee97c0f55793195bf30cc2c619e23769e9
MATIN SANGABI <sangabiwork@gmail.com>
2024-10-06 20:54:33 +0330

    feat : update app in desktop app and install pwa

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -7,17 +7,32 @@ export default function manifest() {
         src: "/web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "maskable",
+        purpose: "any",
       },
       {
         src: "/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "maskable",
+        purpose: "any",
       },
     ],
     theme_color: "#ffffff",
     background_color: "#000000",
     display: "standalone",
+    start_url: "/",
+    screenshots: [
+      {
+        src: "/DESKTOP.png", // برای دسکتاپ
+        sizes: "1916x941",
+        type: "image/png",
+        form_factor: "wide",
+      },
+      {
+        src: "/MOBILE.png", // برای موبایل
+        sizes: "365x793",
+        type: "image/png",
+        form_factor: "narrow", // یا مقدار دیگری به جز wide
+      },
+    ],
   };
 }
